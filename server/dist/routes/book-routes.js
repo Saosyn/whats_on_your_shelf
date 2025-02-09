8800292887f6ffb098cadb2963750348d804ccb2
Josh Kramer <kramer@nyu.edu>
2025-02-09 15:34:09 -0500

    pushing up minor revision to bookpage

--- server/dist/routes/book-routes.js
+++ server/dist/routes/book-routes.js
@@ -2,15 +2,11 @@
 // useful info will be:
 import express from 'express';
 import axios from 'axios';
-// const express = require('express');
-// const axios = require('axios'); // A library for making HTTP requests
 const router = express();
 router.use(express.json());
-// GET route to fetch books from Open Library API
 router.get('/', async (req, res) => {
     try {
-        const searchTerm = req.query.search; // Get the search term from query parameters
-        // Fetch book data from Open Library API using axios
+        const searchTerm = req.query.search;
         const openLibraryResponse = await axios.get(`http://openlibrary.org/search.json?q=${encodeURIComponent(searchTerm)}&fields=title`);
         const responseData = await openLibraryResponse.data;
         // Extract the titles from the response data
